b074bcb68c3ba615bbbf102d81a95f5c39e0a29c
JeremiahUy <jery_3000@hotmail.com>
2019-04-16 12:34:24 +0200

    REFACTOR rendering of privacy model parameters

--- src/components/RenderPrivacyModels.js
+++ src/components/RenderPrivacyModels.js
@@ -3,8 +3,6 @@ import React, { useState } from 'react'
 const PrivacyModelManager = (props) => {
     const { privacyModels } = props;
 
-
-
     let content = (
         <div>
             <table border="1">
@@ -16,15 +14,15 @@ const PrivacyModelManager = (props) => {
                 </thead>
                 <tbody>
                     {privacyModels.map(model => {
-                        //let params = model.params.map((param) => {
-                        //    return Object.keys(param).map((key) => {
-                        //        return ( <tr border="1"><td>{key}</td><td>{param[key]}</td></tr>)
-                        //    })
-                       // })
-                        
-                        return (<tr>
+                            //let params = model.params.map((param) => {
+                            //    return Object.keys(param).map((key) => {
+                            //        return ( <tr border="1"><td>{key}</td><td>{param[key]}</td></tr>)
+                            //    })
+                            // })
+
+                            return (<tr>
                             <td>{model.privacyModel}</td>
-                            <td>{JSON.stringify(model.privacyModel.params)}</td>
+                            <td>{JSON.stringify(model.params)}</td>
                         </tr>)
                     })}
                 </tbody>
