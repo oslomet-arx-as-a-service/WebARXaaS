05f06e0b7969f43d7a576f9e5a0fe26395a528bc
JeremiahUy <jery_3000@hotmail.com>
2019-04-19 13:10:59 +0200

    ADD suppression limit as parameter

--- src/components/RenderSuppressionLimit.js
+++ src/components/RenderSuppressionLimit.js
@@ -12,7 +12,7 @@ const RenderSuppressionLimit = (props) => {
             <br />
             <label> Suppression Limit: {suppressionLimit}</label>
             <br />
-            <button className="btn btn-outline-primary" onClick={() => handleSuppressionLimitRemove()}>Remove</button>
+            <button className="btn btn-danger btn-sm"  onClick={() => handleSuppressionLimitRemove()}>Remove</button>
         </div>
     );
     return content;
