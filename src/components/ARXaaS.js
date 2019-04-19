f1a8b42b5b845cf1bd47b5cbf8d7d6f26e63f950
JeremiahUy <jery_3000@hotmail.com>
2019-04-19 13:14:40 +0200

    REFACTOR to have dafault as null

--- src/components/ARXaaS.js
+++ src/components/ARXaaS.js
@@ -13,7 +13,7 @@ const Anonymise = props => {
   const [currentData, setData] = useState("");
   const [attributes, setAttributes] = useState([]);
   const [privacyModels, setPrivacyModels] = useState([]);
-  const [suppressionLimit, setSuppressionLimit] = useState("");
+  const [suppressionLimit, setSuppressionLimit] = useState(null);
   const [arxResp, setArxResp] = useState('');
   const [action, setAction] = useState('none');
   const attributeTypeModel = 'QUASIIDENTIFYING';
@@ -65,7 +65,7 @@ const Anonymise = props => {
   };
 
   const handleSuppressionLimitRemove = () => {
-    setSuppressionLimit("")
+    setSuppressionLimit(null)
   }
 
   const handleHierarchyUpload = (file, field, index) => {
